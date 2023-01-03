888bd5b1987394ca297f09b7c133ffea14c80bd4
AviGautam1 <avifetian@gmail.com>
2023-01-03 02:54:09 -0800

    Write a Python program to select an item randomly from a list.
    Use random.choice() to get a random element from a given list.

---
+++ Program-24.py
@@ -0,0 +1,10 @@
+# Write a Python program to select an item randomly from a list.
+# Use random.choice() to get a random element from a given list.
+
+
+import random
+
+emp_list = ["avi", "ankur", "deepak", "tanu", "neha"]
+print(random.choice(emp_list))
+
+# output = deepak
