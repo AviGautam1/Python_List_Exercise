f49d8a00b98d5a87ee772f000c585e831c6f2fd6
AviGautam1 <avifetian@gmail.com>
2023-01-02 19:46:36 -0800

    Write a Python program to clone or copy a list.

---
+++ Program-9.py
@@ -0,0 +1,8 @@
+# Write a Python program to clone or copy a list.
+
+old_lst = [2, 3, 4, 5, 12, 8]
+new_lst = list(old_lst)
+
+print("Clone Of The List : ", new_lst)
+
+# Clone Of The List :  [2, 3, 4, 5, 12, 8]
