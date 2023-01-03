d3776371d6e915408f0263c6c005c144c0211989
AviGautam1 <avifetian@gmail.com>
2023-01-02 19:18:23 -0800

    Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.

---
+++ Program-6.py
@@ -0,0 +1,20 @@
+'''
+Write a Python program to get a list, sorted in increasing order by the last element
+in each tuple from a given list of non-empty tuples.
+'''
+
+
+
+def last(n):
+    return n[-1]
+
+def sort(tuples):
+    return sorted(tuples, key=last)
+
+
+#tuples = input("Enter a list of tuples : ")
+
+
+print("Sorted : ", sort([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+
+# output = Sorted :  [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
