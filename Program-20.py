af33fb3e65b805267b8934a91b49da1fc2fb0195
AviGautam1 <avifetian@gmail.com>
2023-01-03 02:33:12 -0800

    Write a Python program to convert a list of characters into a string.

---
+++ Program-20.py
@@ -0,0 +1,16 @@
+# Write a Python program to convert a list of characters into a string.
+
+
+str1 = ['A', 'v', 'i', 'n', 'e', 's', 'h', ' ', 'G', 'a', 'u', 't', 'a', 'm']
+print(type(str1))
+str2 = ''.join(str1)
+print(str2)
+print(type(str2))
+
+
+'''
+output =
+<class 'list'>
+Avinesh Gautam
+<class 'str'>
+'''
