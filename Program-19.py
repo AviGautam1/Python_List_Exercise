fdbf4495fd50f7e7f826a8a1684b20c75a9f4d93
AviGautam1 <avifetian@gmail.com>
2023-01-03 02:27:34 -0800

    Write a Python program access the index of a list.

---
+++ Program-19.py
@@ -0,0 +1,16 @@
+# Write a Python program access the index of a list.
+
+
+emp_names = ["avinesh", "deepak", "ankur", "atul"]
+
+for emp_index, emp_names in enumerate(emp_names):
+    print(emp_index, emp_names)
+
+
+'''
+output =
+0 avinesh
+1 deepak
+2 ankur
+3 atul
+'''
