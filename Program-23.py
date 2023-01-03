58766c74a4eeb32d505bac162b2b4138d8c01f6c
AviGautam1 <avifetian@gmail.com>
2023-01-03 02:50:52 -0800

    Write a Python program to append a list to the second list.

---
+++ Program-23.py
@@ -0,0 +1,12 @@
+# Write a Python program to append a list to the second list.
+
+
+lst1 = ["avi", "ankur", "deepak"]
+lst2 = ["tanu", "neha"]
+
+result_lst = lst1 + lst2
+
+print("After Appending List2 To List1 Is : ", result_lst)
+
+
+# output = After Appending List2 To List1 Is :  ['avi', 'ankur', 'deepak', 'tanu', 'neha']
