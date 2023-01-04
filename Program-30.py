e593c6047033291e39c349f70f6bd3f442298b48
AviGautam1 <avifetian@gmail.com>
2023-01-03 23:22:02 -0800

    Write a Python program to check whether a list contains a sublist.

---
+++ Program-30.py
@@ -0,0 +1,14 @@
+# Write a Python program to check whether a list contains a sublist.
+
+
+lst = [[1, 2, 3], [2, 3, 4], [4, 5, 6]]
+sub_lst = [1, 2, 3]
+
+if sub_lst in lst:
+    print("List Is Present")
+
+else:
+    print("List Is Not Present")
+
+
+# output = List Is Present
